d711acbcc01075d1a1f3afb91040e23fe2710323
Shafaq Saud <shafaq.saud@gmail.com>
2023-03-20 23:08:44 -0400

    Add password for admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  before_action :authenticate
 
   def index
     @products = Product.order(id: :desc).all
@@ -26,6 +27,12 @@ class Admin::ProductsController < ApplicationController
 
   private
 
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+    end
+  end
+
   def product_params
     params.require(:product).permit(
       :name,
@@ -36,5 +43,5 @@ class Admin::ProductsController < ApplicationController
       :price
     )
   end
-
 end
+
