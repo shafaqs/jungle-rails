f0cfc8b45244df280c10473c5bc41001a23a9005
Shafaq Saud <shafaq.saud@gmail.com>
2023-03-21 20:19:45 -0400

    Add categories feature

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,27 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+  def delete
+    @category = Category.delete
+  end
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_path, notice: "Category successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
